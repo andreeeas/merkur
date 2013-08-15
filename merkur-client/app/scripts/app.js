789695dd067e5287e77e4230b79eb86035320417
Andreas Nübel <andreas.nuebel86@googlemail.com>
2013-08-15 22:23:34 +0200

    merkur-client Modul hinzugefuegt.

---
+++ merkur-client/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('merkurClientApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
