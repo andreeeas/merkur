e4281eb3248c487a80d59a72938f3969de7d4b0d
Andreas Nübel <andreas.nuebel86@googlemail.com>
2013-09-02 23:19:31 +0200

    Filtern von Logausgaben hinzugefuegt. Ausgabe angepasst.

--- merkur-client/app/scripts/controllers.js
+++ merkur-client/app/scripts/controllers.js
@@ -3,19 +3,22 @@
 angular.module('merkurClientApp.controllers', [])
   .controller('MainCtrl', ['$scope','stompService','messages.maxEntriesShown','messages.topic',function ($scope, stompService, messagesMaxEntriesShown, messagesTopic) {
     $scope.awesomeThings = [];
+    $scope.query = '';
+    $scope.messagesMaxEntriesShown = messagesMaxEntriesShown;
 
     stompService.client.connect('', '', function(frame) {
       console.log('Connected ' + frame);
       stompService.client.subscribe(messagesTopic, function(message) {
-        $scope.$apply(function () {
-          $scope.awesomeThings.push(message.body);
-          if ($scope.awesomeThings.length > messagesMaxEntriesShown) {
-            $scope.awesomeThings.shift();
-          }
-        });
+        if (message.body.indexOf($scope.query) > -1) {
+          $scope.$apply(function () {
+            $scope.awesomeThings.push(message.body);
+            if ($scope.awesomeThings.length > messagesMaxEntriesShown) {
+              $scope.awesomeThings.shift();
+            }
+          });
+        }
       });
     }, function(error) {
       console.log('STOMP protocol error ' + error);
     });
-
   }]);
