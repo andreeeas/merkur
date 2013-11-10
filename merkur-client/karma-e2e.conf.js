694268179de1d9e2059add4fe3612cbfd60988f2
Andreas Nübel <andreas.nuebel86@googlemail.com>
2013-11-10 14:33:58 +0100

    ChromeCanary entfernt. Firefox hinzugefuegt. Testprofil hinzugefuegt(nur Chrome).

--- merkur-client/karma-e2e.conf.js
+++ merkur-client/karma-e2e.conf.js
@@ -53,7 +53,9 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome','ChromeCanary','Safari'];
+browsers = ['Chrome','Firefox','Safari'];
+// enable this for testing purposes
+//browsers = ['Chrome'];
 
 // If browser does not capture in given timeout [ms], kill it
 captureTimeout = 5000;
